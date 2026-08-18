c26441292b54f4ed7a690fde718b080526aaa2c6
rasm105k <31623093+rasm105k@users.noreply.github.com>
2026-08-18 12:39:31 +0200

    Add root redirect to Pokedex index

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 use App\Http\Controllers\PokemonController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/index', [PokemonController::class, 'index']);
+Route::redirect('/', '/index');
+Route::get('/index', [PokemonController::class, 'index'])->name('index');
 Route::get('/details/{name}', [PokemonController::class, 'details'])->name('details');
 Route::post('/setStarter/{name}', [PokemonController::class, 'setStarter'])->name('setStarter');
 Route::get('/getMyStarters', [PokemonController::class, 'getMyStarters'])->name('getMyStarters');
