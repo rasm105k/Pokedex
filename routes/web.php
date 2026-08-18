fe69e17f2e726d73ccfe0919a8fc9ebd2cc89d28
rasm105k <31623093+rasm105k@users.noreply.github.com>
2026-08-18 12:46:54 +0200

    Keep database-only route fixes

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,9 @@
 use App\Http\Controllers\PokemonController;
 use Illuminate\Support\Facades\Route;
 
-Route::redirect('/', '/index');
-Route::get('/index', [PokemonController::class, 'index'])->name('index');
+Route::get('/index', [PokemonController::class, 'index']);
 Route::get('/details/{name}', [PokemonController::class, 'details'])->name('details');
-Route::post('/setStarter/{name}', [PokemonController::class, 'setStarter'])->name('setStarter');
+Route::get('/setStarter/{name}', [PokemonController::class, 'setStarter'])->name('setStarter');
+
 Route::get('/getMyStarters', [PokemonController::class, 'getMyStarters'])->name('getMyStarters');
+
