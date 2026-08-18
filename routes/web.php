cea988ee21f28d5574283566534ecfcd9a30a3a2
rasm105k <31623093+rasm105k@users.noreply.github.com>
2026-08-18 12:31:13 +0200

    Fix starter routes

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/index', [PokemonController::class, 'index']);
 Route::get('/details/{name}', [PokemonController::class, 'details'])->name('details');
-Route::get('/setStarter/{name}', [PokemonController::class, 'details'])->name('setStarter');
-
-Route::get('/getMyStarters', [PokemonController::class, 'myStarters'])->name('getMyStarters');
-
+Route::post('/setStarter/{name}', [PokemonController::class, 'setStarter'])->name('setStarter');
+Route::get('/getMyStarters', [PokemonController::class, 'getMyStarters'])->name('getMyStarters');
